9a8322c1388eae947134ddc24dbb9507f372eebe
EOlaw <Emmanuel.ao@outlook.com>
2023-12-14 22:03:17 -0600

    Update main.py
    Add comments to the call out functions

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import medical_data_visualizer
 from unittest import main
 
 # Test your function by calling it here
-medical_data_visualizer.draw_cat_plot()
-medical_data_visualizer.draw_heat_map()
+medical_data_visualizer.draw_cat_plot() # Make a cat_plot.png
+medical_data_visualizer.draw_heat_map() # Make a heat_map.png
